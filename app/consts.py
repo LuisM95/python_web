7b9db7e0a317762ee64e6067c2de31a5704abeb0
Luis <martel19.lm@gmail.com>
2020-08-20 13:40:42 +0200

    adding the folder app

---
+++ app/consts.py
@@ -0,0 +1,15 @@
+LOGIN_REQUIRED = 'Es Necesario iniciar Sesion!'
+
+USER_CREATED = 'Usuario Creado Exitosamente!'
+
+LOGOUT = 'Cerraste Sesion!'
+
+ERRO_USER_PASSWORD = 'Usuario o Contrasena Invalidos!'
+
+LOGIN = 'Usuario Autenticado Exitosamente!'
+
+TASK_CREATED = 'Tarea creada Exitosamente!'
+
+TASK_UPDATED = 'Tarea Actualizada!'
+
+TASK_DELETE = "Tarea Eliminada!"
