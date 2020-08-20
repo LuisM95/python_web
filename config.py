1ba8e40052b0787457c459a3a5bf96a246d25549
Luis <martel19.lm@gmail.com>
2020-08-20 13:39:37 +0200

    adding the configurations files

---
+++ config.py
@@ -0,0 +1,30 @@
+from decouple import config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = 'luismartel'
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Sh10n95@localhost/project_web'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:Sh10n95@localhost/project_web_test'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TEST = True
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig,
+    'test': TestConfig
+}
